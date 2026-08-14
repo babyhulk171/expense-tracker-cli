a7fcb66d400e04741217691e1a06fad6bb741861
babyhulk171 <pablo.santosx77@gmail.com>
2026-08-14 19:31:04 -0300

    refactor: delegate more functions to TransactionHandler

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿const string DbName = "finance.db";
+﻿using System.Transactions;
+
+const string DbName = "finance.db";
 var databasePath = Path.Combine(Environment.CurrentDirectory, DbName);
 var database = new TransactionDatabase(databasePath);
 database.Initialize();
@@ -11,30 +13,8 @@ if (args[0] == "delete")
 }
 else if (args[0] == "list")
 {
-    foreach (var transaction in database.GetTransactions())
-    {
-        string sign = transaction.Type == TransactionType.Income ? "+" : "-";
-
-        Console.WriteLine(
-            $"{transaction.Date:dd/MM/yyyy} | " +
-            $"{transaction.Description,-20} | " +
-            $"{sign} R$ {transaction.ValueInCents / 100m:N2} | " +
-            $"{transaction.Category ?? TransactionCategory.Home}");
-    }
-    Environment.Exit(1);
+    TransactionHandler.HandleList(database);
 }
 
-string description = args[0] ?? "";
-decimal value = decimal.Parse(args[1] ?? "0");
-TransactionCategory? category = Enum.Parse<TransactionCategory>(args[2]);
-
-long id = database.AddTransaction(
-    type: TransactionType.Expense,
-    category: category,
-    description: description,
-    value: value,
-    date: DateOnly.FromDateTime(DateTime.Today));
-
-
-Console.WriteLine($"\nTransaction #{id} saved.\n");
+TransactionHandler.HandleAddition(args, database);
 
--- TransactionHandler.cs
+++ TransactionHandler.cs
@@ -1,5 +1,6 @@
 public class TransactionHandler
 {
+    // Delete all the db file and exits.
     public static void HandleDelete(string dbName)
     {
         if (File.Exists(dbName))
@@ -7,5 +8,40 @@ public class TransactionHandler
             File.Delete(dbName);
         }
         Environment.Exit(1);
+    }
+
+    // Iterate over the DB list and write all the transactions and exits.
+    public static void HandleList(TransactionDatabase database)
+    {
+        foreach (var transaction in database.GetTransactions())
+        {
+            string sign = transaction.Type == TransactionType.Income ? "+" : "-";
+
+            Console.WriteLine(
+                $"{transaction.Date:dd/MM/yyyy} | " +
+                $"{transaction.Description,-20} | " +
+                $"{sign} R$ {transaction.ValueInCents / 100m:N2} | " +
+                $"{transaction.Category ?? TransactionCategory.Home}");
+        }
+        Environment.Exit(1);
+    }
+
+    // Parse the command line arguments and add a transaction
+
+    public static void HandleAddition(string[] arguments, TransactionDatabase database)
+    {
+        string description = arguments[0] ?? "";
+        decimal value = decimal.Parse(arguments[1] ?? "0");
+        TransactionCategory? category = Enum.Parse<TransactionCategory>(arguments[2]);
+
+        long id = database.AddTransaction(
+            type: TransactionType.Expense,
+            category: category,
+            description: description,
+            value: value,
+            date: DateOnly.FromDateTime(DateTime.Today));
+
+
+        Console.WriteLine($"\nTransaction #{id} saved.\n");
     }
 }
